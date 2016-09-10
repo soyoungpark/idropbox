aff3c3489bbafeabdeaab86fe2ff370d3a8e343d
SoYoung <soyoung.park05@gmail.com>
2016-09-10 18:19:43 -0400

    port

--- app.js
+++ app.js
@@ -112,6 +112,6 @@ function parseData (data, callback) {
 }
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
